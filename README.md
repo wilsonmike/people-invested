9f2ba06f895fb53a79cd3daf8f46570e4306c405
mike <michaelwilsonsj@hotmail.com>
2022-08-01 03:42:06 -0400

    initial commit - skeleton concept, basic cli interface

---
+++ README.md
@@ -0,0 +1,9 @@
+## People Invested 
+# Provides a data stream of significant asset buying and selling activity
+- Insider Buys
+- Insider Sells
+- Significant mergers & acquisitions 
+- Unusual balance sheet activity
+- Outliers in sector rotation (core investment classes)
+
+Built with GO
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "People Invested"
+	const activeSubLimit = 50
+	var remainingSubs = 50
+	
+	fmt.Printf("Welcome to the %v data stream\n", conferenceName)
+	fmt.Printf("We have a limit of %v total subs and %v are still available\n", activeSubLimit, remainingSubs)
+	fmt.Println("Get your access here")
+
+	var userName string
+	var userSubs int
+	// ask user for their name
+	userName = "Tom"
+	userSubs = 2
+	fmt.Printf("User %v booked %v subs.\n", userName, userSubs)
+}
