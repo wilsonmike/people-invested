f4c10886ea1b40635a46021e6428de89d51a9456
mike <michaelwilsonsj@hotmail.com>
2022-08-02 21:26:41 -0400

    removed log tests for subscription slice data type

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	appName := "People Invested"
 	const activeSubLimit = 50
 	var remainingSubs uint = 50
-	var subscriptions []string 
+	subscriptions := []string{} 
 
 	fmt.Printf("Welcome to the %v data stream\n", appName)
 	fmt.Printf("We have a limit of %v total subs and %v are still available\n", activeSubLimit, remainingSubs)
@@ -30,14 +30,10 @@ func main() {
 	fmt.Scan(&userSubs)
 
 	remainingSubs = remainingSubs - userSubs 
-	subscriptions[0] = firstName + " " + lastName 
 	subscriptions = append(subscriptions, firstName + " " + lastName)
-
-	fmt.Printf("The whole array: %v\n", subscriptions)	
-	fmt.Printf("The first value: %v\n", subscriptions[0])	
-	fmt.Printf("Array type: %T\n", subscriptions)
-	fmt.Printf("Array length: %v\n", len(subscriptions))	
 	
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userSubs, email)
 	fmt.Printf("%v subs remaining for %v\n", remainingSubs, appName)
+
+	fmt.Printf("Thank you to all of our active users: %v", subscriptions)
 }
