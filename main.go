29c597fc6b42831f4bb40cebc4f6ddc1575eefc8
mike <michaelwilsonsj@hotmail.com>
2022-08-06 13:16:41 -0400

    added string field to split first names and end loop if all available subs are taken

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+) 
 
 func main() {
 	appName := "People Invested"
@@ -12,28 +15,42 @@ func main() {
 	fmt.Printf("We have a limit of %v total subs and %v are still available\n", activeSubLimit, remainingSubs)
 	fmt.Println("Get your access here")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userSubs uint
-	// ask user for their name
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userSubs uint
+		// ask user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
 
-	fmt.Println("How many subscriptions would you like to purchase?: ")
-	fmt.Scan(&userSubs)
+		fmt.Println("How many subscriptions would you like to purchase?: ")
+		fmt.Scan(&userSubs)
 
-	remainingSubs = remainingSubs - userSubs 
-	subscriptions = append(subscriptions, firstName + " " + lastName)
-	
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userSubs, email)
-	fmt.Printf("%v subs remaining for %v\n", remainingSubs, appName)
+		remainingSubs = remainingSubs - userSubs 
+		subscriptions = append(subscriptions, firstName + " " + lastName)
+		
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userSubs, email)
+		fmt.Printf("%v subs remaining for %v\n", remainingSubs, appName)
 
-	fmt.Printf("Thank you to all of our active users: %v", subscriptions)
+		firstNames := []string{}
+		for _, booking := range subscriptions {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		} 
+
+		fmt.Printf("Thank you to all of our active users: %v\n", firstNames)
+		
+		var noSubsRemaining bool = remainingSubs == 0
+		if noSubsRemaining {
+			fmt.Println("Oops, looks like we are out of available subscriptions this month.")
+			break
+		}
+	}
 }
